ea09622af5bd0531922a9eeb8ee6fb715d63165e
zhao745399842 <745399842@qq.com>
2018-07-07 21:25:42 +0800

    测试冲突2

--- src/main/java/com/my/test/privader/TestCommit.java
+++ src/main/java/com/my/test/privader/TestCommit.java
@@ -3,7 +3,8 @@ package com.my.test.privader;
 public class TestCommit {
 
     public static void main(String[] args){
-        System.out.println("你好我是第四次修改");
+        //测是冲突
+        System.out.println("你好我是第6次修改");
     }
 
 

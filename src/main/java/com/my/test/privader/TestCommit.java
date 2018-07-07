53a72f9d14c09ee65ee9e3db97125b9f7f7cbcbe
zhaowei39 <zhaowei39@jd.com>
2018-07-07 21:20:46 +0800

    测试冲突

--- src/main/java/com/my/test/privader/TestCommit.java
+++ src/main/java/com/my/test/privader/TestCommit.java
@@ -3,7 +3,7 @@ package com.my.test.privader;
 public class TestCommit {
 
     public static void main(String[] args){
-        System.out.println("你好我是第二次修改");
+        System.out.println("你好我是第四次修改");
     }
 
 

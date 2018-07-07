d05449561b7a71f4416a640dad57978f81fbfa12
zhao745399842 <745399842@qq.com>
2018-07-07 21:12:32 +0800

    Update TestCommit.java

--- src/main/java/com/my/test/privader/TestCommit.java
+++ src/main/java/com/my/test/privader/TestCommit.java
@@ -3,7 +3,7 @@ package com.my.test.privader;
 public class TestCommit {
 
     public static void main(String[] args){
-        System.out.println("dddd");
+        System.out.println("你好我是第二次修改");
     }
 
 

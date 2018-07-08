4af89ba719fdd4dc1a7d44018500654e79fa09b4
zhaowei39 <zhaowei39@jd.com>
2018-07-08 15:00:42 +0800

    测试idea

--- src/main/java/com/my/test/privader/TestCommit.java
+++ src/main/java/com/my/test/privader/TestCommit.java
@@ -3,7 +3,7 @@ package com.my.test.privader;
 public class TestCommit {
 
     public static void main(String[] args){
-        //测是冲突解决冲突
+        //测是冲突解决
         System.out.println("你好我是第8次修改");
 
     }

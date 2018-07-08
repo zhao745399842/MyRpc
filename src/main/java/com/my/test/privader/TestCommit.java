e2c4b606f3dbe7656772a1e01fc0670adf3f5402
zhaowei39 <zhaowei39@jd.com>
2018-07-08 14:50:35 +0800

    提交测试

--- src/main/java/com/my/test/privader/TestCommit.java
+++ src/main/java/com/my/test/privader/TestCommit.java
@@ -3,8 +3,8 @@ package com.my.test.privader;
 public class TestCommit {
 
     public static void main(String[] args){
-        //测是冲突222
-        System.out.println("你好我是第7次修改");
+        //测是冲突解决冲突
+        System.out.println("你好我是第8次修改");
 
     }
 
